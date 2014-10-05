9b9f0039bb6a428c22a26a8e25229a5b395eed93
Abhiroop Dabral <abhiroop11@yahoo.com>
2014-10-05 03:33:29 -0400

    Adding Project 2 Base files
    in folder Project 2

---
+++ Project 2/CSP/CSP/sudokuSolver.py
@@ -0,0 +1,36 @@
+# SUDOKU SOLVER
+
+import sys
+from time import time
+from sudokuUtil import *
+
+# Please implement function solve_puzzle
+# input puzzle: 2D list, for example:
+# [ [0,9,5,0,3,2,0,6,4]
+#   [0,0,0,0,6,0,1,0,0]
+#   [6,0,0,0,0,0,0,0,0]
+#   [2,0,0,9,0,3,0,0,6]
+#   [0,7,6,0,0,0,0,0,3]
+#   [3,0,0,0,0,0,0,0,0]
+#   [9,0,0,5,0,4,7,0,1]
+#   [0,5,0,0,2,1,0,9,0]
+#   [0,0,8,0,0,6,3,0,5] ]
+# Return a 2D list with all 0s replaced by 1 to 9.
+# You can utilize argv to distinguish between algorithms
+# (basic backtracking or with MRV and forward checking).
+# For example: python sudokuSolver.py backtracking
+def solve_puzzle(puzzle, argv):
+    """Solve the sudoku puzzle."""
+    return load_sudoku('given_solution.txt')
+
+#===================================================#
+puzzle = load_sudoku('puzzle.txt')
+
+print "solving ..."
+t0 = time()
+solution = solve_puzzle(puzzle, sys.argv)
+t1 = time()
+print "completed. time usage: %f" %(t1 - t0), "secs."
+
+save_sudoku('solution.txt', solution)
+
