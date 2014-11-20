059893e5b939fc7807aa5d9186c3f087e6c8ac12
rishijosan <josanrishi@gmail.com>
2014-11-20 16:30:40 -0500

    Q5 commented

--- Project 3/BayesianNet/question5_solver.py
+++ Project 3/BayesianNet/question5_solver.py
@@ -24,10 +24,12 @@ class Question5_Solver:
 	
     def solve(self, query):
         
+        #Adding ` to beginning and end of word to simplify logic
         tmpWord = '``' + query + '``'
 
         index = -1
         
+        #Search for index of _ in word
         for char in tmpWord:
             index += 1
             if char == "_":
@@ -35,16 +37,20 @@ class Question5_Solver:
         
         #print "Break!!!"
         
+        
+        #Characters before and after _
         before1 = tmpWord[index-2]
         before2 = tmpWord[index-1]
         after1 = tmpWord[index+1]
         after2 = tmpWord[index+2]
         
         #print "Before, After : " , before, after
-            
+         
+        #Variables to keep track of maximum probability and the characters     
         maxProb = -1
         maxChar = "a"
         
+        #Loop over all alphabets and find the character that results in highest probability
         for i in xrange(97,123): 
             char = chr(i)
             currProb = self.cpt2.conditional_prob(char, before1, before2) * self.cpt2.conditional_prob(after1 , before2, char) * self.cpt2.conditional_prob(after2 , char, after1) 
