d7f3b3823fa5e67e5e5a4e1349e7000eb2d45527
rishijosan <josanrishi@gmail.com>
2014-11-19 20:34:28 -0500

    Q5 Done

--- Project 3/BayesianNet/question5_solver.py
+++ Project 3/BayesianNet/question5_solver.py
@@ -19,7 +19,39 @@ class Question5_Solver:
     #    self.cpt2.conditional_prob("`", "b", "`");
     # query example:
     #    query: "ques_ion";
-    #    return "t";
+    #    return "t"
+	
+	
     def solve(self, query):
-        return "t";
+        
+        tmpWord = '``' + query + '``'
+
+        index = -1
+        
+        for char in tmpWord:
+            index += 1
+            if char == "_":
+                break
+        
+        #print "Break!!!"
+        
+        before1 = tmpWord[index-2]
+        before2 = tmpWord[index-1]
+        after1 = tmpWord[index+1]
+        after2 = tmpWord[index+2]
+        
+        #print "Before, After : " , before, after
+            
+        maxProb = -1
+        maxChar = "a"
+        
+        for i in xrange(97,123): 
+            char = chr(i)
+            currProb = self.cpt2.conditional_prob(char, before1, before2) * self.cpt2.conditional_prob(after1 , before2, char) * self.cpt2.conditional_prob(after2 , char, after1) 
+            if currProb > maxProb:
+                maxProb = currProb
+                maxChar = char
+                
+
+        return maxChar;
 
