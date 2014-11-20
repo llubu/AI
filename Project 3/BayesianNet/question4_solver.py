b311e7ac675f399ab3c22e40c68cdd9d778229e6
Abhiroop Dabral <abhiroop11@yahoo.com>
2014-11-19 21:59:05 -0500

    Q4 Done

--- Project 3/BayesianNet/question4_solver.py
+++ Project 3/BayesianNet/question4_solver.py
@@ -1,6 +1,9 @@
 class Question4_Solver:
     def __init__(self, cpt):
         self.cpt = cpt;
+        self.skip1 = self.createTable1()
+        self.skip2 = self.createTable2()
+
 
     #####################################
     # ADD YOUR CODE HERE
@@ -16,5 +19,102 @@ class Question4_Solver:
     #            "in_elligence", "inter--_"];
     #    return "t";
     def solve(self, query):
-        return "t";
+        
+        uS = [0 for i in range(0, 4)]
+        prevDash = [0 for i in range(0, 4)]
+        afterDash = [0 for i in range(0, 4)]
+        leftch = [0 for i in range(0, 4)]
+        rtch = [0 for i in range(0, 4)]
+        
+        #print '\n'
+        #print uS, prevDash, afterDash, leftch, rtch
+        
+        for i in range(0, 4):
+            tmp = '`' + query[i] + '`'
+            uS[i] = tmp.index('_')
+            tmp1 = tmp.split('_')
+            #print tmp
+            prevDash[i] = tmp1[0].count('-')
+            afterDash[i] = tmp1[1].count('-')
+            
+        
+            if  0 != (uS[i] - prevDash[i]):
+                leftch[i] = tmp[uS[i]-prevDash[i] -1]
+            else:
+                leftch[i] = tmp[uS[i]-prevDash[i]]
+                
+            if len(tmp) != (uS[i] + afterDash[i]):
+                rtch[i] = tmp[uS[i]+afterDash[i] +1]
+            else:
+                rtch[i] = tmp[uS[i]+afterDash[i]]
+                
+        #print prevDash, ':' , afterDash, ':' , leftch, ':', rtch
+        
+        leftsum = 0
+        rtsum = 0
+        maxSum = -1
+        maxChar = '?'
+        
+        for i in range(0, 26):
+            ksum = 1
+            
+            for k in range(0, 4):
+                if (0 == prevDash[k]):
+                    leftsum = self.cpt.conditional_prob(chr(97+i) ,leftch[k])
+                if (1 == prevDash[k]):
+                    leftsum = self.skip1[chr(97+i), leftch[k]]
+                if (2 == prevDash[k]):
+                    leftsum = self.skip2[chr(97+i), leftch[k]]
+                    
+                if (0 == afterDash[k]):
+                    rtsum = self.cpt.conditional_prob(rtch[k], chr(97+i))
+                if (1 == afterDash[k]):
+                    rtsum = self.skip1[rtch[k], chr(97+i)]
+                if (2 == afterDash):
+                    rtsum = self.skip2[rtch[k], chr(97+i)]
+                    
+                ksum *= leftsum*rtsum
+                
+            if ( maxSum < ksum ):
+                maxSum = ksum
+                maxChar = chr(97+i)
+                
+        return maxChar;
+    
+    
+    def createTable1(self):
+        """
+        Creates the lookup table to help eliminate hidden var 1
+        """
+        table= dict()
+        
+        for i in range(0,27):
+            prev = chr(96+i)
+            for j in range(0, 27):
+                sum = 0
+                cur = chr(96+j)
+                for k in range(0, 27):
+                    sum += self.cpt.conditional_prob(chr(96+k), prev) * self.cpt.conditional_prob(cur, chr(96+k))
+                    
+                table[prev,cur] = sum
+                
+        return table
+        
+    def createTable2(self):
+        """
+        Creates the lookup table to help eliminate hidden var 2
+        """
+        table = dict()
+        
+        for i in range(0, 27):
+            prev = chr(96+i)
+            for j in range(0, 27):
+                sum = 0
+                cur = chr(96+j)
+                for k in range(0, 27):
+                    sum += self.skip1[chr(96+k), prev] * self.cpt.conditional_prob(cur, chr(96+k))
+                
+                table[prev, cur] = sum
+        return table
+
 
