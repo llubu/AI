ba007dadd0a3b382a1e4bae63dd8d9ef11998088
rishijosan <josanrishi@gmail.com>
2014-11-30 09:15:24 -0500

    Adding Project 4 files

---
+++ Project 4/machine_learning/question1_solver.py
@@ -0,0 +1,20 @@
+class Question1_Solver:
+    def __init__(self):
+        self.learn('train.data');
+        return;
+
+    # Add your code here.
+    # Read training data and build your decision tree
+    # Store the decision tree in this class
+    # This function runs only once when initializing
+    # Please read and only read train_data: 'train.data'
+    def learn(self, train_data):
+        return;
+
+    # Add your code here.
+    # Use the learned decision tree to predict
+    # query example: 'n,y,n,y,y,y,n,n,n,y,?,y,y,y,n,y'
+    # return 'republican' or 'republican'
+    def solve(self, query):
+        return 'democrat';
+
---
+++ Project 4/machine_learning/question3_solver.py
@@ -0,0 +1,11 @@
+class Question3_Solver:
+    def __init__(self):
+        return;
+
+    # Add your code here.
+    # Return the centroids of clusters.
+    # You must use [(30, 30), (150, 30), (90, 130)] as initial centroids
+    def solve(self, points):
+        centroids = [(30, 60), (150, 60), (90, 130)];
+        return centroids;
+
