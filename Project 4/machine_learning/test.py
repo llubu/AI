5d8aca64aaff00c2ac5506b27e73a87eca864a96
rishijosan <josanrishi@gmail.com>
2014-12-02 17:10:04 -0500

    Recursive code, problems with attributes

--- Project 4/machine_learning/test.py
+++ Project 4/machine_learning/test.py
@@ -1,6 +1,40 @@
 import math
 import sys
 
+choices = ['Y', 'N', '?']
+    
+def readData():
+    with open("train.data", "r") as f:
+        data = f.read().splitlines();
+        
+    #print len(data), len(data[0])
+    sanData = list()
+    
+    for item in data:
+        resRow = list()
+        params = list()
+        
+        row = item.split('\t')
+        resRow.append(row[0])
+        
+        vals = row[1].split(',')
+        i = 0
+        
+        for res in vals:
+            if res == 'y':
+                params.append('y')
+            if res == 'n':
+                params.append('n')
+            if res == '?':
+                params.append('?')        
+        i += 1
+        
+        resRow.append(params)
+        sanData.append(resRow)
+        
+    return sanData
+
+    
 def compEntropy(table):
     
     #print table
@@ -24,104 +58,106 @@ def compEntropy(table):
         entropy = 0
     
     #print probRep, probDem
-    
     return entropy
+
+
+
+#Function to compute best attribute, given a subset of the training data 
+#Return index of best attribute and subsets after split on that attribute   
+def bestAttr(curData):
+
+    #Compute current entropy 
+    curEnt = compEntropy([ row[0] for row in curData ])
+    #print curEnt
+
+    #Vars to store index of attribute which minimized entropy
+    minEnt = sys.maxint 
+    minInd = -1
+
+    #Lists to store y,n,? subsets for attribute which minimizes entropy
+    splitY = []
+    splitN = []
+    splitX = []
+
+    #Compute Entropy for all attributes and find the attribute that leads to max drop in entropy
+    for i in range(len(curData[0][1])):
+        #print i
+        tabY = []
+        tabN = []
+        tabX = []
+
+        #Split records on the basis of y,n,? into respective tables
+        for j in range(len(curData)):
+            if curData[j][1][i] == 'y':
+                tabY.append(curData[j])
+            elif curData[j][1][i] == 'n':
+                tabN.append(curData[j])
+            elif curData[j][1][i] == '?':
+                tabX.append(curData[j])
+        
+        subY = [row[0] for row in tabY]
+        subN = [row[0] for row in tabN]
+        subX = [row[0] for row in tabX]
+        
+        #Compute Entropy for this attribute
+        totRecords = len(subY) + len(subN) + len(subX)
+        attrEnt = ((float(len(subY))/totRecords) *  compEntropy(subY)) + ((float(len(subN))/totRecords) *  compEntropy(subN)) + ((float(len(subX))/totRecords) *  compEntropy(subX))
+
+        #print "attrEnt: ", attrEnt
+        #print ""
+        
+        if attrEnt < minEnt:
+            minEnt = attrEnt
+            minInd = i
+            
+            splitY = tabY
+            splitN = tabN
+            splitX = tabX
+            
+    #print minInd
+
+    #print "Before: " , len(splitY[0][1])
+    #We have to remove the attribute at minInd
+    for i in range(len(splitY)):
+        del splitY[i][1][minInd]
+        #splitY[i][1][minInd] = "NULL"
+    for i in range(len(splitN)):
+        del splitN[i][1][minInd]
+        #splitN[i][1][minInd] = "NULL"
+    for i in range(len(splitX)):
+        del splitX[i][1][minInd]
+        #splitX[i][1][minInd] = "NULL"        
+        
+    #print "After: " , len(splitY[0][1])
+    return minInd, [splitY, splitN, splitX]
+
+
+
+#Function to create decision tree
+def createDecisionTree(data):
     
-    
-with open("train.data", "r") as f:
-    data = f.read().splitlines();
-    
-print len(data), len(data[0])
-
-sanData = list()
-
-for item in data:
-
-    resRow = list()
-    params = list()
-    
-    row = item.split('\t')
-    resRow.append(row[0])
-    
-    vals = row[1].split(',')
-    
-    i = 0
-    for res in vals:
-        if res == 'y':
-            params.append('y')
-        if res == 'n':
-            params.append('n')
-        if res == '?':
-            params.append('?')        
-    i += 1
-    
-    resRow.append(params)
-    sanData.append(resRow)
-
-curData = sanData
-    
-
-
-#Compute current entropy 
-curEnt = compEntropy([ row[0] for row in curData ])
-print curEnt
-
-#Vars to store index of attribute which minimized entropy
-minEnt = sys.maxint 
-minInd = -1
-
-#Lists to store y,n,? subsets for attribute which minimizes entropy
-splitY = []
-splitN = []
-splitX = []
-
-#Compute Entropy for all attributes and find the attribute that leads to max drop in entropy
-for i in range(len(curData[0][1])):
-    print i
-    tabY = []
-    tabN = []
-    tabX = []
-
-    #Split records on the basis of y,n,? into respective tables
-    for j in range(len(curData)):
-        if curData[j][1][i] == 'y':
-            tabY.append(curData[j])
-        elif curData[j][1][i] == 'n':
-            tabN.append(curData[j])
-        elif curData[j][1][i] == '?':
-            tabX.append(curData[j])
-    
-    subY = [row[0] for row in tabY]
-    subN = [row[0] for row in tabN]
-    subX = [row[0] for row in tabX]
-    
-    #Compute Entropy for this attribute
-    totRecords = len(subY) + len(subN) + len(subX)
-    attrEnt = ((float(len(subY))/totRecords) *  compEntropy(subY)) + ((float(len(subN))/totRecords) *  compEntropy(subN)) + ((float(len(subX))/totRecords) *  compEntropy(subX))
-
-    print "attrEnt: ", attrEnt
+    print "Len data: " , len(data)
+    print data
     print ""
+    if len(data) == 0 or data[0][1].count("NULL") == 16:
+        return data
+        
+    bAttr, subSet = bestAttr(data)
+    decTree = {bAttr:{}}
     
-    if attrEnt < minEnt:
-        minEnt = attrEnt
-        minInd = i
-        
-        splitY = tabY
-        splitN = tabN
-        splitX = tabX
-
-        
-print minInd
-
-#We have to remove the attribute at minInd
-for i in range(len(splitY)):
-    del splitY[i][1][minInd]
-for i in range(len(splitN)):
-    del splitN[i][1][minInd]
-for i in range(len(splitX)):
-    del splitX[i][1][minInd]    
-
-
+    print bAttr
+    
+    #print subSet[0]
+    #print len(subSet[0][0][1])
+    
+    for i in range(len(choices)):
+        subTree = createDecisionTree(subSet[i])
+        decTree[bAttr][choices[i]] = subTree
+    
+    return decTree
+    
+sanData = readData()  
+createDecisionTree(sanData)
 
 
 #We now have 3 subsets to recurse on    
